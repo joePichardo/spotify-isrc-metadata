f2c5df2c72977a9dd719b7e036b149aefa6ecfe2
joePichardo <joepichar@gmail.com>
2022-11-30 13:39:21 -0700

    Add User and Track Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from "./auth/auth.module";
+import { UserModule } from './user/user.module';
+import { TrackModule } from './track/track.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, UserModule, TrackModule],
 })
 export class AppModule {}

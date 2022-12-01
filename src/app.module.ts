f398bab29f4026d59af92544674325ef46e5bf06
joePichardo <joepichar@gmail.com>
2022-12-01 12:32:03 -0700

    refactor using ConfigModule for env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { TrackModule } from './track/track.module';
 import { PrismaModule } from './prisma/prisma.module';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     AuthModule,
     UserModule,
     TrackModule,
     PrismaModule,
-    ConfigModule.forRoot({ isGlobal: true }),
   ],
 })
 export class AppModule {}

6104e18d1460b9d9d0faae3e0148dc315cecb140
joePichardo <joepichar@gmail.com>
2022-11-30 23:56:58 -0700

    add Prisma and env ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from "./auth/auth.module";
+import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { TrackModule } from './track/track.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UserModule, TrackModule],
+  imports: [
+    AuthModule,
+    UserModule,
+    TrackModule,
+    PrismaModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
 })
 export class AppModule {}
